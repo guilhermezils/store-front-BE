19827c3574a2b63780a5b0955f4dcf27bfb2d04f
guilhermezils <guilhermezils@icloud.com>
2021-06-21 21:30:03 -0500

    authBE

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :index]
       post '/login', to: 'auth#create'
+      # post '/login', to: 'users#login'
       get '/profile', to: 'users#profile'
     end
   end
